c9ebefdc08a3812c64d28c43cf9bfd6f66030e6d
Julian Rodriguez <julian@sonetica.net>
2015-05-09 22:04:25 -0400

    editted footer to include contact form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+Webstore::Application.config.secret_key_base = '91b341d6c4accb5bfaacae34654429b5c2da529820ea1524e3a8ac8d2ade2765ce49a86a906cf44b8e1d8a0e2c97e707c29f237a467ec0f2cc2cd85376cdf827' # replace this
