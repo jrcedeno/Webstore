b601c7214edb62cff1d65d21e0d1652748503e34
Julian Rodriguez <julian@sonetica.net>
2015-05-11 17:52:47 -0400

    setup log in and out functionality in the navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,6 @@ Webstore::Application.routes.draw do
 
   # ACTION '/ADDRESS'  =>      'CONTROLLER#METHOD'
 
-  get '/about'       =>      'static_pages#about'
-  get '/faq'         =>      'static_pages#faq'
-  get '/html_css'    =>      'static_pages#html_css'
-  get '/java'        =>      'static_pages#java'
-  get '/rails'       =>      'static_pages#rails'
-  get '/javascript'  =>      'static_pages#javascript'
-
-  get '/sub'         =>      'subscriptions#new'
-  post'/sub'         =>      'subscriptions#subscribe'
-
-  get '/contact'     =>      'static_pages#contact'
-  post '/contact'    =>      'static_pages#contacted_us'
-
-  resources :courses
 
   
 end
