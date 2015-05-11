4a4af5c8249f287631243b10505b53f4ebc9d086
Julian Rodriguez <julian@sonetica.net>
2015-05-11 18:25:12 -0400

    editted environment files to update app mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Webstore::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
 end
