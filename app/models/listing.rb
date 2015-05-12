7c535820bcee0b927c85661d200d5dd8b4ee3bdc
Julian Rodriguez <julian@sonetica.net>
2015-05-12 19:13:33 -0400

    updated listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,12 @@
 class Listing < ActiveRecord::Base
+
+	has_attached_file :image, :styles => { :medium => "300x300>" }
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	validates :image, :attachment_presence => true
+	validates :title, presence: true
+	validates :artist, presence: true
+	validates :description, presence: true
+	validates :price, presence: true
+
 end
